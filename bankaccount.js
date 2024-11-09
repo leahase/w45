18e9f55e22b6134311fa7919869b202419137604
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-09 15:35:29 +0100

    instructions

---
+++ bankaccount.js
@@ -0,0 +1,79 @@
+// BANK ACCOUNT 
+
+ 
+
+// REQUIREMENTS 
+
+// Create an object called account that has the following properties: 
+
+// accountName, should be the data type string 
+
+// This property should contain the name of the account holder 
+
+ 
+
+// - balance, should be the data type number 
+
+// this property should contain the total amount of the account 
+
+ 
+
+//  - getBalance, should be a function 
+
+// this function should display the total amount of the account to the user 
+
+ 
+
+//  - deposit, also a function 
+
+//  this function should be able to deposit money onto the balance of the account 
+
+ 
+
+//  - withdrawal, also a function 
+
+//  this function should be able do withdrawal money from the balance of the account 
+
+ 
+
+//  - getAccountName, function as well 
+
+//  this function should dispaly the account holders name to the user 
+
+ 
+
+//  - accountError, same as above function! 
+
+//  this one is a bit tricky... it's up to you to figure out how or what you should use this for. 
+
+//  HINT: it's more a thinking poblem than a technical problem :) 
+
+ 
+
+// EXTRA: exitAccount, should be a function 
+
+//  this function should exit the account 
+
+//  HINT: there are a few different ways to do this, it's up to you which way you choose. 
+
+ 
+
+//  EXTRA = OPTIONAL NOT MANDATORY 
+
+ 
+
+//  Remember that a function is just a value. And if a function is just a 
+
+//  value then we can both pass it as a parameter to a function and 
+
+//  pass it as a property of an object. 
+
+ 
+
+//  The object should handle all of the functionality (logic) 
+
+ 
+
+//  The atm() function should be responsible for showing the user interface 
+
+//  and based on the user input show the right meny choice 
